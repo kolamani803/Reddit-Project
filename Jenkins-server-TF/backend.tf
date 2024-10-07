6fcf1e5ec14e128ea1b2b0411af4a88cf6b0ddb3
kolamani803 <74824088+kolamani803@users.noreply.github.com>
2024-10-08 00:03:09 +0530

    Update backend.tf

--- Jenkins-server-TF/backend.tf
+++ Jenkins-server-TF/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "my-reddit-bucket-1"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
